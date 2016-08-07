10d0a98447c7e1840b6a01234edb45819ff75daa
noprom <tyee.noprom@qq.com>
2016-08-07 13:06:00 +0800

    optimize download php

--- download.php
+++ download.php
@@ -1,16 +1,17 @@
 <?php
 
-if ($argc != 3) {
-    echo 'Usage: ' . __FILE__ . ' package_name destination_data_dir' . PHP_EOL;
-    echo 'Eg: php download.php com.tencent.mtt.intl ~/Desktop' . PHP_EOL;
+if ($argc != 4) {
+    echo 'Usage: ' . __FILE__ . ' conf_file package_name destination_data_dir' . PHP_EOL;
+    echo 'Eg: php download.php ~/Desktop/crawler.conf com.tencent.mtt.intl ~/Desktop' . PHP_EOL;
     return;
 }
 
-$package = $argv[1];
-$dataDir = $argv[2];
-$cmd = `java -jar target/googleplaycrawler-0.3-jar-with-dependencies.jar -f ~/Desktop/crawler.conf download $package | grep curl`;
+$conf = $argv[1];
+$package = $argv[2];
+$data_dir = $argv[3];
+$cmd = `java -jar target/googleplaycrawler-0.3-jar-with-dependencies.jar -f $conf download $package | grep curl`;
 $cmd = trim($cmd) . " | grep Location";
 $cmd = `$cmd`;
 $cmd = explode("Location: ", $cmd);
-$cmd = "curl \"" . trim($cmd[1]) . "\" > " . $dataDir . "/" . $package . ".apk";
+$cmd = "curl \"" . trim($cmd[1]) . "\" > " . $data_dir . "/" . $package . ".apk";
 `$cmd`;
